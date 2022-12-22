197af4374e752b2c699f2ad569f5d11a61dd0fdf
Atabaev Rahimzhan <rahimatabaev75@mail.ru>
2022-12-22 16:57:57 +0600

    First task completed

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -10,22 +10,26 @@ public class Main {
 
         var cats = Cat.makeCats(10);
         Printer.print(cats);
+
         cats.sort(Comparator.comparing(Cat::getName));
         Printer.print(cats);
+        System.out.println("В данной таблице выполнена сортировка по имени. \n\n");
 
         cats.sort(Comparator.comparing(Cat::getAge));
         Printer.print(cats);
+        System.out.println("В данной таблице выполнена сортировка по возрасту. \n\n");
 
         cats.sort(Comparator.comparing(Cat::getBreed));
         Printer.print(cats);
+        System.out.println("В данной таблице выполнена сортировка по -Breed-\n\n");
 
         cats.sort(Comparator.comparing(Cat::getColor));
         cats.removeIf(cats1 -> cats1.getColor().equals(Cat.Color.GRAY));
         Printer.print(cats);
 
-//        cats.sort(Comparator.comparing());
+        cats.removeIf(cats2 -> cats2.getName().length()==5);
+        System.out.println("В данной таблице показаны коты у которых длина имени меньше 5 символов:\n\n");
+        Printer.print(cats);
 
     }
-
-
 }
